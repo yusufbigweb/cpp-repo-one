1f2f5094ea8e617ad58a3815ab072657305b99e3
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-10-16 07:54:47 +0000

    Update

---
+++ one.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Preprocessor directive for input-output stream
+
+using namespace std;  // Allows usage of names for objects and variables from the standard library
+
+int main() {
+    cout << "Hello, World!" << endl;  // Outputs "Hello, World!" followed by a newline
+    return 0;  // Returns 0 to indicate that the program ended successfully
+}
